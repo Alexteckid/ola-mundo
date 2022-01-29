202ce8c393c6a1ade426cf2bca1f228970b83996
Alexteckid <98231416+Alexteckid@users.noreply.github.com>
2022-01-29 20:43:34 -0300

    Add files via upload

---
+++ bstraindo um Jogo de RPG Usando Orientação a Objetos com C#.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using obj.Debug.src.entites;
+
+namespace obj
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Arus hero = new Arus("Arus", 23, "kinght");
+
+          
+
+            Console.WriteLine( hero);
+        }
+    }
+}
